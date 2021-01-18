0171b90d72630fa18012c735dc0e9896e0395cbe
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2021-01-18 18:58:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+camera=document.getElementById("camera");
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+Webcam.attach('#camera');
+
+console.log('ml5 version:',ml5.version);
+
+function take_snapshot(){
+ Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'"/>'
+    });
+}
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/le3N75XoU/model.json',ModelLoaded);
+
+function ModelLoaded() {
+    console.log('Model Loaded!')
+}
